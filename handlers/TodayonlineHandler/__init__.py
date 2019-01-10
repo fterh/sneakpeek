93133cac65959c0e4f0c6b34bc8174baefc51cba
Fabian <fabianterh@gmail.com>
2019-01-10 13:53:50 +0800

    Fix stylistic inconsistencies
    Also delete unused import statements

--- handlers/TodayonlineHandler/__init__.py
+++ handlers/TodayonlineHandler/__init__.py
@@ -1,19 +1,19 @@
+import json
+import requests
+
+from bs4 import BeautifulSoup
+
 from comment import Comment
 from handlers.AbstractBaseHandler import AbstractBaseHandler, HandlerError
 
-import requests
-import re
-from bs4 import BeautifulSoup
-import textwrap
-import json
 
 class TodayonlineHandler(AbstractBaseHandler):
     @classmethod
     def handle(cls, url):
         html = requests.get(url).text
         soup = BeautifulSoup(html, "html.parser")
-        articleid = soup.find("meta", {"name":"cXenseParse:recs:articleid"})["content"]
-        TODAY_API_URL = f"https://www.todayonline.com/api/v3/article/{articleid}"
+        article_id = soup.find("meta", {"name":"cXenseParse:recs:articleid"})["content"]
+        TODAY_API_URL = f"https://www.todayonline.com/api/v3/article/{article_id}"
         article = json.loads(requests.get(TODAY_API_URL).content)["node"]
         title = article["title"]
         body = BeautifulSoup(article["body"], "html.parser").get_text()
