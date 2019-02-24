af5645b9484c55b6c42401c3f56dc07992ef8981
changhuapeng <changhuapeng@users.noreply.github.com>
2019-02-25 01:23:55 +0800

    Update HandlerManager to take in consideration subdomains

--- handler.py
+++ handler.py
@@ -1,43 +1,62 @@
 import tldextract
 
+from collections import defaultdict
+
 from handlers.ArticleHandler import ArticleHandler
 from handlers.RicemediaHandler import RicemediaHandler
-from handlers.TodayonlineHandler import TodayonlineHandler
 from handlers.STHandler import STHandler
+from handlers.TodayonlineHandler import TodayonlineHandler
 
 
 class HandlerManager:
     """
-    Manage Handlers and map domain names to Handlers.
+    Manage Handlers and map subdomains and domain names to Handlers.
     """
     handlers = {
-        "channelnewsasia.com": ArticleHandler,
-        "mothership.sg": ArticleHandler,
-        "ricemedia.co": RicemediaHandler,
-        "straitstimes.com": STHandler,
-        "todayonline.com": TodayonlineHandler,
-        "zula.sg": ArticleHandler
+        "channelnewsasia.com": defaultdict(
+            lambda: ArticleHandler # Default Handler for all subdomains (unless specify) for channelnewsasia.com
+        ),
+        "mothership.sg": defaultdict(
+            lambda: ArticleHandler
+        ),
+        "ricemedia.co": defaultdict(
+            lambda: RicemediaHandler
+        ),
+        "straitstimes.com": defaultdict(
+            lambda: STHandler
+        ),
+        "todayonline.com": defaultdict(
+            lambda: TodayonlineHandler
+        ),
+        "zula.sg": defaultdict(
+            lambda: ArticleHandler
+        )
     }
 
     @classmethod
     def extract_hostname(cls, url):
-        """Extract hostname from URL."""
+        """Extract subdomain and domain name from URL."""
         url_extract = tldextract.extract(url)
-        return url_extract.domain + "." + url_extract.suffix
+        domains = {
+            "subdomain": url_extract.subdomain,
+            "domain_name": url_extract.domain + "." + url_extract.suffix
+        }
+        return domains
 
     @classmethod
     def has_handler(cls, url):
         """Check if a submission URL has an appropriate Handler."""
-        domain_name = cls.extract_hostname(url)
-        return domain_name in cls.handlers
+        domains = cls.extract_hostname(url)
+        return domains["domain_name"] in cls.handlers
 
     @classmethod
     def get_handler(cls, url):
         """
-        Return the Handler corresponding to a domain name.
-        Raise ValueError if no such Handler exists.
+        Return the Handler corresponding to a subdomain and domain name.
+        Will return the default domain Handler if subdomain Handler do not exist.
+        Raise ValueError if no domain Handler exists.
         """
         if not cls.has_handler(url):
             raise ValueError("Domain name {} has no Handler".format(url))
-        domain_name = cls.extract_hostname(url)
-        return cls.handlers[domain_name]
+        domains = cls.extract_hostname(url)
+        return cls.handlers[domains["domain_name"]][domains["subdomain"]]
