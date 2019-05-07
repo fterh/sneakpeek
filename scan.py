0a09a9445cf351beb836af4d7a312bbefeea6e58
Fabian <fabianterh@gmail.com>
2019-05-07 17:07:55 +0800

    Implement logging
    * Replace print statements with logging statements
    * Add logging
    * Possibly fix bugs ("return" statement within for loop replaced with "continue" statement)

--- scan.py
+++ scan.py
@@ -1,6 +1,5 @@
-import traceback
-
 import config
+import logging
 import sys
 from handler import HandlerManager
 from comment import format_comment
@@ -9,46 +8,64 @@ from qualify import qualify
 
 def scan(subreddit):
     """Scan a Subreddit for new submissions."""
-    print("Starting scan")
+    logging.info("Starting scan")
 
     for submission in subreddit.stream.submissions(skip_existing=True):
-        print("Operating on submission ID: " + submission.id)
+        logging.info("Operating on submission")
+        logging.debug("Submission ID = {}, title = {}".format(
+            submission.id,
+            submission.title
+        ))
 
         does_qualify = qualify(submission)
 
         if does_qualify:
-            print("Submission qualifies")
+            logging.info("Submission qualifies")
 
-            handler = HandlerManager.get_handler(submission.url)
+            handler = None
+            try:
+                logging.info("Attempting to get article handler")
+                handler = HandlerManager.get_handler(submission.url)
+                logging.info("Article handler = {}".format(handler))
+            except Exception as e:
+                logging.error("""
+                    An error occurred while getting article handler. This should never happen.
+                    """)
+                logging.error("Exception = {}".format(e))
+                logging.info("Skipping current submission")
+                continue
 
             comment_raw = None
             try:
+                logging.info("Attempting to generate raw comment using handler")
                 comment_raw = handler.handle(submission.url)
+                logging.info("Raw comment generated")
             except Exception as e:
-                print(f"Exception occurred while handling {submission.url}")
-                traceback.print_exc()
+                logging.error("An error occurred while handling URL = {}".format(
+                    submission.url
+                ))
+                logging.error("Exception = {}".format(e))
+                logging.info("Skipping current submission")
+                continue
 
             if comment_raw is None:
-                skip(submission)
-                return
+                logging.error("comment_raw is None; skipping current submission")
+                continue
+
+            logging.info("Generating formatted comment")
             comment_markdown = format_comment(comment_raw)
+            logging.info("Formatted comment generated")
 
             if len(comment_markdown) < config.COMMENT_LENGTH_LIMIT:
                 try:
-                    print("Attempting to post a comment")
+                    logging.info("Attempting to post comment")
                     submission.reply(comment_markdown)
-                    print("Comment posting succeeded")
+                    logging.info("Comment posting succeeded")
                 except Exception as e:
-                    print("An error occurred:")
-                    print(e)
+                    logging.error("An error occurred while posting comment")
+                    logging.error("Exception = {}".format(e))
             else:
-                print("Submission is too long to be posted.")
+                logging.warning("Submission is too long to be posted")
         else:
-            skip(submission)
-
-        # Flush stdout buffer
-        sys.stdout.flush()
-
-def skip(submission):
-    # If submission does not qualify, write SKIP to database only if it is new.
-    print("Submission does not qualify")
+            logging.info("Comment does not qualify; skipping comment")
+            continue
