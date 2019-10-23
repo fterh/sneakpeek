63fcc3c45f65d4c21a9722ae9d61b4a25b098d93
Wesley <wesleykok-@hotmail.com>
2019-10-23 11:49:01 +1000

    Replace numbers with config variable

--- scan.py
+++ scan.py
@@ -70,13 +70,12 @@ def scan(subreddit):
             try:
                 logging.info("Attempting to post two comments")
                 i = 1
-                while (comment_markdown[9999-i] != " "):
+                while (comment_markdown[config.COMMENT_LENGTH_LIMIT-i] != " "):
                     i += 1  
-                    part_1 = comment_markdown[0: 10000-i]
-                    part_2 = comment_markdown[10000-i:]
+                    part_1 = comment_markdown[0: config.COMMENT_LENGTH_LIMIT-i]
+                    part_2 = comment_markdown[config.COMMENT_LENGTH_LIMIT-i:]
                 if (part_2[0] != ">"):
-                    part_2 = ">" + part_2
-                    
+                    part_2 = ">" + part_2     
                 first_comment = submission.reply(part_1)
                 first_comment.reply(part_2)
                 logging.info("Comments posting succeeded")
