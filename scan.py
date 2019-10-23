1608d8fc099d55a5fa51bd217d01c98d6f2a3692
Wesley <wesleykok-@hotmail.com>
2019-10-23 17:07:51 +1000

    more trailing whitespace

--- scan.py
+++ scan.py
@@ -71,7 +71,7 @@ def scan(subreddit):
                 logging.info("Attempting to post two comments")
                 i = 1
                 while (comment_markdown[config.COMMENT_LENGTH_LIMIT-i] != " "):
-                    i += 1                    
+                    i += 1
                 part_1 = comment_markdown[0: config.COMMENT_LENGTH_LIMIT-i]
                 part_2 = comment_markdown[config.COMMENT_LENGTH_LIMIT-i:]
                 if (part_2[0] != ">"):
