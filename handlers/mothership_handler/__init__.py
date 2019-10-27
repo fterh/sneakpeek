354692c90d2d2b9a8a528cf093a2144aa6cb185c
Wesley <wesleykok-@hotmail.com>
2019-10-28 08:49:14 +1000

    Add Mothership handler/cleanup

---
+++ handlers/mothership_handler/__init__.py
@@ -0,0 +1,21 @@
+"""Handler for Mothership."""
+from newspaper import Article
+from comment import Comment
+from handlers.abstract_base_handler import AbstractBaseHandler, HandlerError
+
+
+class MothershipHandler(AbstractBaseHandler):
+    """Handler for Mothership."""
+    @classmethod
+    def handle(cls, url):
+        article = Article(url)
+        article.download()
+        article.parse()
+        title = article.title
+        body = article.text
+
+        # replace parsed arguments and trailing ads
+        body = body.replace('\nAdvertisement\n', '')
+        body = body.partition('Content that keeps Mothership.sg going')
+
+        return Comment(title, body)
